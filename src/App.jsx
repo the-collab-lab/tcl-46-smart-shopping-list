93b3d5a7d1db5590ab385fd450ca64e1c21ac7ad
h-yung <helen.yung.h@gmail.com>
2022-07-11 11:27:04 -0400

    some changes for nav redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 
 import { AddItem, Home, Layout, List } from './views';
 
