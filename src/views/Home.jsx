85285dc1268e92690373441eff67692c4b458c28
h-yung <helen.yung.h@gmail.com>
2022-07-11 10:37:48 -0400

    use custom hook to set new token, remove test list

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,14 @@
 import './Home.css';
 
-export function Home() {
+export function Home(props) {
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
+			<button type="button" onClick={props.makeList}>
+				Make new list
+			</button>
 		</div>
 	);
 }
