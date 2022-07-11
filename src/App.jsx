deeb0f46f49d559b0bc059add638ec57a4da9648
Yuffie Hu <yuffieevans@gmail.com>
2022-07-11 10:25:42 -0500

    feat: redirect to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 
 import { AddItem, Home, Layout, List } from './views';
 
@@ -73,7 +78,16 @@ export function App() {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route index element={<Home makeList={makeNewList} />} />
+					<Route
+						index
+						element={
+							listToken ? (
+								<Navigate to="/list" />
+							) : (
+								<Home makeList={makeNewList} />
+							)
+						}
+					/>
 					<Route path="/list" element={<List data={data} />} />
 					<Route path="/add-item" element={<AddItem />} />
 				</Route>
