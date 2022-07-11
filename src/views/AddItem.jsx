9a44d7c4aa1558fedeee262a60d9300ee6c96cce
Xinrui Chen <xynree@gmail.com>
2022-07-11 16:22:46 -0700

    Basic jsx layout for `AddItem`

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,21 @@
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	return (
+		<div>
+			<p>
+				Hello from the <code>/add-item</code> page!
+			</p>
+			<form onSubmit={() => {}}>
+				<label for="item">
+					Add Item
+					<input placeholder="my form" id="addItem" />
+				</label>
+				<select>
+					<option value={7}>Soon</option>
+					<option value={14}>Kind of Soon</option>
+					<option value={30}>Not Soon</option>
+				</select>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
