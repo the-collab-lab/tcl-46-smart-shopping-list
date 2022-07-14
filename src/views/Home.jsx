bfd5bb56d55386445bb8592588c368d6631db3c0
Yuffie Hu <yuffieevans@gmail.com>
2022-07-13 22:15:37 -0500

    change comments

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,18 +4,13 @@ import { generateToken } from '@the-collab-lab/shopping-list-utils';
 
 export function Home({ makeNewList }) {
 	const navigate = useNavigate();
+
+	// use callback function so the newToken that's generated in this child component
+	// can be passed up to the parent component (App.js)
 	function handleClick() {
 		const newToken = generateToken();
 		makeNewList(newToken);
 		navigate('/list');
-		/**
-		 * Check local storage for token
-		 * -- if none,*/
-
-		// update value of listToken
-
-		// BUG? token value updates in localStorage when function is called onClick in Firefox browser
-		// but localStorage token does not update in Chrome onClick, although key *will* update 1x in new browser session for Chrome
 	}
 
 	return (
