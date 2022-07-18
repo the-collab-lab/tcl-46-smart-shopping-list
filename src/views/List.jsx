118d2e68cc40c55e72ea64927652c552ed65c4ce
Andrew D <56039221+andent2@users.noreply.github.com>
2022-07-18 15:13:07 -0400

    Add filter functoinality

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,15 +1,51 @@
+import { useState } from 'react';
+
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const clearSearchTerm = () => {
+		setSearchTerm('');
+	};
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+
+			<form>
+				<label>
+					Filter Items
+					<input
+						type="text"
+						placeholder="start typing here..."
+						id="filter"
+						name="filter"
+						value={searchTerm}
+						onChange={(e) => {
+							setSearchTerm(e.target.value);
+						}}
+					/>
+				</label>
+			</form>
+			<button onClick={clearSearchTerm}>clear</button>
+
 			<ul>
-				{data.map(({ name, id }) => (
-					<ListItem name={name} key={id} />
-				))}
+				{data
+					.filter((data) => {
+						if (searchTerm == '') {
+							return data.name;
+						} else if (
+							data.name.toLowerCase().includes(searchTerm.toLowerCase())
+						) {
+							return data;
+						}
+					})
+					.map(({ name, id }) => (
+						<ListItem name={name} key={id} />
+					))}
 			</ul>
 		</>
 	);
