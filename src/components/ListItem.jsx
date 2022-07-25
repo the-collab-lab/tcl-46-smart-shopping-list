0930ccb069a632bcf32f8da0346d603dabdcf8ce
Andrew D <56039221+andent2@users.noreply.github.com>
2022-07-25 15:54:50 -0400

    Add checkbox to list items

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,18 @@
 import './ListItem.css';
 
 export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+	return (
+		<li className="ListItem">
+			<input
+				className="ListItem-checkbox"
+				type="checkbox"
+				id={name}
+				name={name}
+				value={name}
+			/>
+			<label className="ListItem-label" htmlFor={name}>
+				{name}
+			</label>
+		</li>
+	);
 }
