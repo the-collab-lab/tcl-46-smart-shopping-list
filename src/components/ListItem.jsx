125b429ed248d48da471065a9014036c291e1e19
Andrew D <56039221+andent2@users.noreply.github.com>
2022-07-25 16:07:15 -0400

    Set up state for checkbox

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from 'react';
+
 import './ListItem.css';
 
 export function ListItem({ name }) {
+	const [isChecked, setIsChecked] = useState(false);
+
+	useEffect(() => {
+		console.log(isChecked);
+	}, [isChecked]);
+
+	function handleValueChange(evt) {
+		setIsChecked(evt.target.checked);
+	}
+
 	return (
 		<li className="ListItem">
 			<input
@@ -9,6 +21,7 @@ export function ListItem({ name }) {
 				id={name}
 				name={name}
 				value={name}
+				onChange={handleValueChange}
 			/>
 			<label className="ListItem-label" htmlFor={name}>
 				{name}
