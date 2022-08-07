ddbf490b0bf4452b09318aa13eeeed6fb2f97419
h-yung <helen.yung.h@gmail.com>
2022-08-07 14:39:07 -0400

    start inc change

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -124,14 +124,21 @@ export async function updateItem(
 
 	const listCollectionRef = collection(db, listId);
 	const listItemRef = doc(listCollectionRef, itemId);
-	if (isChecked) {
+
+	if (isChecked /*&& currentTime > dateLastPurchased.toMillis() + 50000*/) {
 		await updateDoc(listItemRef, {
 			dateLastPurchased: currentDate,
 			isChecked: isChecked,
 			totalPurchases: increment(1),
-			dateNextPurchased: actualDateNextPurchased, //updated with
+			dateNextPurchased: actualDateNextPurchased,
 		});
+		/*} else if (isChecked && currentTime < dateLastPurchased.toMillis() + 50000){
+		console.log('not updating bc too soon')
+		await updateDoc(listItemRef, {
+			isChecked: isChecked,
+		});*/
 	} else {
+		// not checked at all
 		await updateDoc(listItemRef, {
 			isChecked: isChecked,
 		});
