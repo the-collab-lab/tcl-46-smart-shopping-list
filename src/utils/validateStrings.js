58c5bb1f0cd0747047d93a263f3bae0c23680ca3
Xinrui Chen <xyn.hello@gmail.com>
2022-08-07 09:00:00 -0700

    added comments and updated to return `undefined` on invalid inputs

--- src/utils/validateStrings.js
+++ src/utils/validateStrings.js
@@ -1,16 +1,35 @@
 /**
  * Checks if an empty string of any length has been passed in
  * @param {string} itemName
- * @returns {boolean}
+ * @returns {boolean | undefined}
  */
 export const isEmpty = (itemName) =>
-	typeof itemName !== 'string' || itemName.trim().length === 0;
+	typeof itemName === 'string' ? itemName.trim().length === 0 : undefined;
 
-export const isDuplicate = (itemName, data) =>
-	data.some(({ name }) => cleanup(name) === cleanup(itemName));
+/**
+ * Checks if a name exists in an array of item objects, allowing for fuzziness
+ * @param {string} itemName
+ * @param {{name: string}[]} data
+ * @returns {boolean | undefined}
+ */
+export const isDuplicate = (itemName, data) => {
+	if (
+		typeof itemName !== 'string' ||
+		!data ||
+		!Array.isArray(data) ||
+		!data.every((val) => val.hasOwnProperty('name'))
+	)
+		return undefined;
 
-export const cleanup = (inputString) =>
-	inputString
-		.toLowerCase()
-		.trim()
-		.replace(/[\W_-]/g, '');
+	return data.some(({ name }) => cleanup(name) === cleanup(itemName));
+};
+
+/**
+ * Normalizes by lowercasing and replacing any non word character including _ and - with ''
+ * @param {string} inputString
+ * @returns {string | undefined}
+ */
+export const cleanup = (itemName) =>
+	typeof itemName === 'string'
+		? itemName.toLowerCase().replace(/[\W_-]/g, '')
+		: undefined;
