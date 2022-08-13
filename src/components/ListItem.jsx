2cd4cf59d23cb80bc12a07de768cd216a1f43a4f
h-yung <helen.yung.h@gmail.com>
2022-08-12 22:44:43 -0400

    trying alternative urgency labeling

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,6 +4,8 @@ import { updateItem } from '../api/firebase';
 
 import { getFutureDate } from '../utils';
 
+import { checkIfActive } from '../utils/item';
+
 import './ListItem.css';
 
 export function ListItem({
@@ -15,12 +17,64 @@ export function ListItem({
 	dateLastPurchased,
 	dateNextPurchased,
 	totalPurchases,
-	urgency,
-	urgencyMessage,
+	refTime,
+	daysToNext,
 }) {
 	const DAYINMS = 86400000;
 	// sync up checked or not checked data from the database to the page upon page refresh
 	const [isPurchased, setIsPurchased] = useState(isChecked);
+
+	const [urgency, setUrgency] = useState('');
+	const [urgencyMessage, setUrgencyMessage] = useState('');
+
+	// would prefer the following to work:
+	// inside rendered component would have been: className={urgency[urgencyIndex][0]}, <p>{urgency[urgencyIndex][1]}</p>
+
+	// const urgency = [
+	// 	['overdue', 'Overdue'],
+	// 	['soon', 'Soon'],
+	// 	['kind-of-soon','Kind Of Soon'],
+	// 	['not-soon', 'Not Soon'],
+	// 	['inactive','Inactive']
+	// ]
+	// const [urgencyIndex, setUrgencyIndex] = useState('');
+
+	// useEffect(()=> {
+	// 	console.log('am I looping?')
+	// 	let currentTime = new Date().getTime();
+	// 	console.log(`active? ${checkIfActive(refTime,currentTime)}`)
+	// 	if (!checkIfActive(refTime,currentTime)){
+	// 		setUrgencyIndex(4)
+	// 	} else if (daysToNext < 0) {
+	// 		setUrgencyIndex(0)
+	// 	} else if (daysToNext <= 7){
+	// 		setUrgencyIndex(1)
+	// 	} else if (daysToNext > 7 && daysToNext < 30){
+	// 		setUrgencyIndex(2)
+	// 	} else if (daysToNext >= 30){
+	// 		setUrgencyIndex(3);
+	// 	}
+	// }, [refTime, daysToNext])
+
+	useEffect(() => {
+		let currentTime = new Date().getTime();
+		if (!checkIfActive(refTime, currentTime)) {
+			setUrgency('inactive');
+			setUrgencyMessage('Inactive');
+		} else if (daysToNext < 0) {
+			setUrgency('overdue');
+			setUrgencyMessage('Overdue');
+		} else if (daysToNext <= 7) {
+			setUrgency('soon');
+			setUrgencyMessage('Soon');
+		} else if (daysToNext > 7 && daysToNext < 30) {
+			setUrgency('kind-of-soon');
+			setUrgencyMessage('Kind Of Soon');
+		} else if (daysToNext >= 30) {
+			setUrgency('not-soon');
+			setUrgencyMessage('Not Soon');
+		}
+	}, [refTime, daysToNext]);
 
 	useEffect(() => {
 		if (isChecked !== isPurchased) {
@@ -47,6 +101,7 @@ export function ListItem({
 			}
 		}
 	}, []);
+
 	function handleValueChange(evt) {
 		setIsPurchased(evt.target.checked);
 	}
--- src/utils/item.js
+++ src/utils/item.js
@@ -13,14 +13,8 @@ export const sortByNextDateAlphabetical = (array) =>
 		(a, b) => a.daysToNext - b.daysToNext || a.name.localeCompare(b.name),
 	);
 
-export function comparePurchaseUrgency(data) {
-	/**with the following behaviors
-	sorts inactive items last, then
-	sorts items in ascending order of days until purchase, and
-	sorts items with the same days until purchase alphabetically */
-
-	const dataWithDays = data.map((item) => {
-		// we will use these frequently in the next ops, so passing as additional keys:
+const appendDataToDays = (data) =>
+	data.map((item) => {
 		let refTime = item.dateLastPurchased
 			? item.dateLastPurchased.toMillis()
 			: item.dateCreated.toMillis();
@@ -32,61 +26,23 @@ export function comparePurchaseUrgency(data) {
 		return { ...item, refTime, daysToNext };
 	});
 
-	const sortedOverdueList = sortByNextDateAlphabetical(
-		dataWithDays
-			.filter((item) => {
-				const { refTime, daysToNext } = item;
-				return checkIfActive(refTime, currentTime) && daysToNext < 0;
-			})
-			.map((item) => {
-				let urgency = 'overdue';
-				let urgencyMessage = 'Overdue';
-				return { ...item, urgency, urgencyMessage };
-			}),
+const sortedMultiArr = (dataWithDays) =>
+	dataWithDays.reduce(
+		(sorted, item) => {
+			if (!checkIfActive(item.refTime, currentTime)) {
+				sorted[2].push(item);
+			} else if (
+				checkIfActive(item.refTime, currentTime) &&
+				item.daysToNext < 0
+			) {
+				sorted[0].push(item);
+			} else sorted[1].push(item);
+			return sorted;
+		},
+		[[], [], []],
 	);
 
-	const sortedActiveListNoOverdues = sortByNextDateAlphabetical(
-		dataWithDays
-			.filter((item) => {
-				const { refTime, daysToNext } = item;
-				return checkIfActive(refTime, currentTime) && daysToNext >= 0;
-			})
-			.map((item) => {
-				let urgency;
-				let urgencyMessage;
-
-				const { daysToNext } = item;
-				if (daysToNext < 0) {
-					urgency = 'overdue';
-					urgencyMessage = 'Overdue';
-				} else if (daysToNext <= 7) {
-					urgency = 'soon';
-					urgencyMessage = 'Soon';
-				} else if (daysToNext > 7 && daysToNext < 30) {
-					urgency = 'kind-of-soon';
-					urgencyMessage = 'Kind Of Soon';
-				} else if (daysToNext >= 30) {
-					urgency = 'not-soon';
-					urgencyMessage = 'Not Soon';
-				}
-
-				return { ...item, urgency, urgencyMessage };
-			}),
-	);
-	const sortedInactiveListByDateNext = sortByNextDateAlphabetical(
-		dataWithDays
-			.filter((item) => {
-				const { refTime } = item;
-				return !checkIfActive(refTime, currentTime);
-			})
-			.map((item) => {
-				let urgency = 'inactive';
-				let urgencyMessage = 'Inactive';
-				return { ...item, urgency, urgencyMessage };
-			}),
-	);
-	return sortedOverdueList.concat(
-		sortedActiveListNoOverdues,
-		sortedInactiveListByDateNext,
-	);
-}
+export const comparePurchaseUrgency = (data) => {
+	const dataWithDays = appendDataToDays(data);
+	return sortedMultiArr(dataWithDays).map(sortByNextDateAlphabetical).flat();
+};
