093c5914ee4b5432418609dba9959459fddf15c6
Xinrui Chen <xyn.hello@gmail.com>
2022-08-14 14:42:36 -0700

    Rebased and updated with main

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { deleteItem } from '../api';
 import { ListItem } from '../components';
