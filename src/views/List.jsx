4ce7f56c3628da3b5be4122f73918279e0b2641a
h-yung <helen.yung.h@gmail.com>
2022-08-16 16:55:32 -0400

    added custom date range filter

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -5,15 +5,36 @@ import { ListItem } from '../components';
 import { comparePurchaseUrgency, getUrgency } from '../utils/item';
 import NoToken from '../components/NoToken';
 
+const defaultDates = { startDate: '', endDate: '' };
+
 export function List({ data, listToken, setListToken }) {
 	const navigate = useNavigate();
 	const [searchTerm, setSearchTerm] = useState('');
 	const [copied, setCopied] = useState('');
-
-	// temp
 	const [urgencyTerm, setUrgencyTerm] = useState('ALL');
+	const [custom, setCustom] = useState(defaultDates);
 
 	const sortedFullList = useMemo(() => comparePurchaseUrgency(data), [data]);
+
+	const updateRange = (e) => {
+		if (e.target.name !== 'startDate' && e.target.name !== 'endDate') return;
+		console.log(`range is updating with ${e.target.name}: ${e.target.value}`);
+		const newDates = { ...custom, [e.target.name]: e.target.value };
+		setCustom(newDates);
+	};
+	// place in a useEffect? but they are already in state vars - do they need to be? maybe just chain filter
+	// alternately, run this function onChange for either the two inputs?
+	const customDateRange = (start, end) => {
+		// input values are yyyy-mm-dd strings (custom.startDate, custom.endDate)
+
+		if (!start || !end) return () => true;
+		const start_IN_MS = new Date(start).getTime();
+		const end_IN_MS = new Date(end).getTime();
+		console.log('I am running too');
+		return (item) =>
+			item.dateNextPurchased.toMillis() >= start_IN_MS &&
+			item.dateNextPurchased.toMillis() < end_IN_MS;
+	};
 
 	useEffect(() => {
 		if (copied) setTimeout(() => setCopied(''), 2000);
@@ -61,8 +82,7 @@ export function List({ data, listToken, setListToken }) {
 	};
 
 	const handleChange = (e) => {
-		setUrgencyTerm(e.target.value); //async? do I need?
-		//passed to getUrgency
+		setUrgencyTerm(e.target.value); //this is passed to getUrgency
 	};
 
 	return (
@@ -83,25 +103,58 @@ export function List({ data, listToken, setListToken }) {
 								}}
 							/>
 						</label>
-						<label>
-							Show by urgency
-							{/* some redundancy compared to full storage in listItem? */}
-							<select
-								value={urgencyTerm}
-								onChange={(e) => handleChange(e)}
-								name="urgency"
-							>
-								<option value="ALL">Choose urgency</option>
-								<option value="SOON">Soon</option>
-								<option value="KIND_OF_SOON">Kind Of Soon</option>
-								<option value="NOT_SOON">Not Soon</option>
-								<option value="OVERDUE">Overdue</option>
-								<option value="INACTIVE">Inactive</option>
-							</select>
-						</label>
 						<button type="button" onClick={clearSearchTerm} aria-live="polite">
 							clear
 						</button>
+						<fieldset>
+							<legend>Search by custom purchase-by date range</legend>
+							{/* are these input types accessible? */}
+							<label>
+								Start date:
+								<input
+									type="date"
+									value={custom.startDate}
+									name="startDate"
+									max={custom.endDate}
+									onChange={updateRange}
+								/>
+							</label>
+							<label>
+								End date:
+								<input
+									type="date"
+									value={custom.endDate}
+									name="endDate"
+									min={custom.startDate}
+									onChange={updateRange}
+								/>
+							</label>
+							<button
+								type="button"
+								onClick={() => setCustom(defaultDates)}
+								aria-live="polite"
+							>
+								Clear custom range
+							</button>
+						</fieldset>
+						<div>
+							<label>
+								Show by urgency
+								{/* some redundancy compared to full storage in listItem? */}
+								<select
+									value={urgencyTerm}
+									onChange={(e) => handleChange(e)}
+									name="urgency"
+								>
+									<option value="ALL">Choose urgency</option>
+									<option value="SOON">Soon</option>
+									<option value="KIND_OF_SOON">Kind Of Soon</option>
+									<option value="NOT_SOON">Not Soon</option>
+									<option value="OVERDUE">Overdue</option>
+									<option value="INACTIVE">Inactive</option>
+								</select>
+							</label>
+						</div>
 						<div>
 							<p>
 								Copy token to allow others join your list:
@@ -114,6 +167,7 @@ export function List({ data, listToken, setListToken }) {
 							{filterList(sortedFullList)
 								.filter((item) => item.name !== '')
 								.filter(getUrgency(urgencyTerm))
+								.filter(customDateRange(custom.startDate, custom.endDate))
 								.map((item) => (
 									<ListItem
 										{...item}
