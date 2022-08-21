8b50816cfed85eaaf00784910a5bff3d86218efb
Yuffie Hu <yuffieevans@gmail.com>
2022-08-21 00:11:56 -0500

    temp CSS mockup

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet, NavLink } from 'react-router-dom';
 import FoodPuns from '../components/FoodPuns';
+import { useState } from 'react';
 
 import './Layout.css';
 
 export function Layout() {
+	const [activeNavLink, setActiveNavLink] = useState('');
+	console.log(activeNavLink);
 	return (
 		<div className="Layout">
 			<header className="Layout-header">
@@ -14,18 +17,44 @@ export function Layout() {
 			</main>
 			<FoodPuns />
 			<nav className="Nav">
-				<NavLink to="/" className="Nav-link">
+				<NavLink
+					to="/"
+					// className={({ isActive }) => {
+					// 	if (isActive) setActiveNavLink('home');
+					// 	return 'Nav-link';
+					// }}
+					className="Nav-link"
+				>
 					Home
 				</NavLink>
-				<NavLink to="/list" className="Nav-link">
+				<NavLink
+					to="/list"
+					className={({ isActive }) => {
+						if (isActive) setActiveNavLink('list');
+						return 'Nav-link';
+					}}
+				>
 					List
 				</NavLink>
-				<NavLink to="/add-item" className="Nav-link">
+				<NavLink
+					to="/add-item"
+					className={({ isActive }) => {
+						if (isActive) setActiveNavLink('add-item');
+						return 'Nav-link';
+					}}
+				>
 					Add Item
 				</NavLink>
-				<NavLink to="/summary" className="Nav-link">
+				<NavLink
+					to="/summary"
+					className={({ isActive }) => {
+						if (isActive) setActiveNavLink('summary');
+						return 'Nav-link';
+					}}
+				>
 					Summary
 				</NavLink>
+				<div className="Nav-link__shadow"></div>
 			</nav>
 		</div>
 	);
