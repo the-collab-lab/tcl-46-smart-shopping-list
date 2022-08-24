b3549606aaa5905a7473f4f38046d10bbc57500a
h-yung <helen.yung.h@gmail.com>
2022-08-24 14:07:04 -0400

    placeholder UI and function added in Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -16,6 +16,7 @@ export function Home() {
 	const [listName, setListName] = useState('');
 	const [userList, setUserList] = useContext(MyContext).userListCtx;
 	const [listToken, setListToken] = useContext(MyContext).listTokenCtx;
+	const [data] = useContext(MyContext).dataCtx;
 
 	const navigate = useNavigate();
 
@@ -74,6 +75,34 @@ export function Home() {
 			setListToken(getFirstToken(JSON.parse(updatedList)));
 		}
 	};
+
+	const deleteListFake = () => {
+		if (
+			window.confirm(
+				'Are you sure you want to delete your shopping list? This cannot be undone.',
+			)
+		) {
+			const itemsToBeDeleted = [];
+			data.forEach((item) => {
+				console.log(`item to remove: ${item.name}`);
+				itemsToBeDeleted.push(item);
+				// itemsToBeDeleted.push(deleteItem(listToken, item.id));
+			});
+			// Promise.all(itemsToBeDeleted)
+			// 	.catch((err) => {
+			// 		console.log(err);
+			// 	})
+			// 	.finally(() => {
+			// 		const updatedList = removeList(
+			// 			userList,
+			// 			getMatchingName(userList, listToken),
+			// 		);
+			// 		setUserList(updatedList);
+			// 		setListToken(getFirstToken(JSON.parse(updatedList)));
+			// 		navigate('/');
+			// 	});
+		}
+	};
 	return (
 		<div className="Home">
 			<form onSubmit={makeNewList}>
@@ -101,7 +130,11 @@ export function Home() {
 				<button type="submit">Submit</button>
 			</form>
 			{errorMessage && <p>{errorMessage}</p>}
-			<ListSwitcher switchList={switchList} rmListUpdate={rmListUpdate} />
+			<ListSwitcher
+				switchList={switchList}
+				rmListUpdate={rmListUpdate}
+				deleteListFake={deleteListFake}
+			/>
 			<Summary />
 		</div>
 	);
