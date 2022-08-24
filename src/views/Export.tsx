1df94b99e5141475219ff4f06b70b1fc03c7d2cc
h-yung <helen.yung.h@gmail.com>
2022-08-24 15:15:05 -0400

    moved isvalidtoken to utils

--- src/views/Export.tsx
+++ src/views/Export.tsx
@@ -3,15 +3,11 @@ import { useContext, useEffect } from 'react';
 import { MyContext } from '../App';
 import { CopyToken } from '../components/CopyToken';
 import NoToken from '../components/NoToken';
+import { isValidToken } from '../utils';
 export function Export() {
 	const [adjustedData] = useContext(MyContext).adjustedDataCtx;
 	const [listToken, setListToken] = useContext(MyContext).listTokenCtx;
 	const [userList] = useContext(MyContext).userListCtx;
-
-	const isValidToken = (token) => {
-		const regexPattern = /(?:\w+ ){2}\w+/;
-		return regexPattern.test(token);
-	};
 
 	return (
 		<>
