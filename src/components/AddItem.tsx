e1212ff8cb51fed3358d911534cd11a186014885
h-yung <helen.yung.h@gmail.com>
2022-08-25 15:58:49 -0400

    styling list no items scenario

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react';
 import { MyContext } from '../App';
-import { isEmpty, isDuplicate } from '../utils/validateStrings';
+import { isEmpty, isDuplicate, isValidToken } from '../utils/validateStrings';
 import { addItem } from '../api/firebase';
 import NoToken from './NoToken';
 import { getUserListsArr } from '../utils/user';
@@ -57,35 +57,37 @@ export function AddItem() {
 
 	return (
 		<>
-			{listToken && listToken !== 'null' ? (
+			{listToken && isValidToken(listToken) ? (
 				<div className="AddItem">
-					<form onSubmit={addItemToDatabase}>
-						<label htmlFor="addItem" className="addItem__label">
-							Add:
-							<input
-								placeholder="Item name"
-								id="addItem"
-								value={item.itemName}
-								onChange={updateItem}
-								name="itemName"
-								className="addItem__input"
-							/>
-						</label>
-						<label htmlFor="itemFrequency" className="addItem__label">
-							Frequency:
-							<select
-								value={item.daysUntilNextPurchase}
-								onChange={updateItem}
-								name="daysUntilNextPurchase"
-								id="itemFrequency"
-								className="addItem__select_frequency"
-							>
-								<option value={7}>Soon</option>
-								<option value={14}>Kind of Soon</option>
-								<option value={30}>Not Soon</option>
-							</select>
-						</label>
-						<div>
+					<form className="addItem__form" onSubmit={addItemToDatabase}>
+						<div className="addItem__section">
+							<label htmlFor="addItem" className="addItem__label">
+								Add:
+								<input
+									placeholder="Item name"
+									id="addItem"
+									value={item.itemName}
+									onChange={updateItem}
+									name="itemName"
+									className="addItem__input"
+								/>
+							</label>
+							<label htmlFor="itemFrequency" className="addItem__label">
+								Frequency:
+								<select
+									value={item.daysUntilNextPurchase}
+									onChange={updateItem}
+									name="daysUntilNextPurchase"
+									id="itemFrequency"
+									className="addItem__select addItem__select_frequency"
+								>
+									<option value={7}>Soon</option>
+									<option value={14}>Kind of Soon</option>
+									<option value={30}>Not Soon</option>
+								</select>
+							</label>
+						</div>
+						<div className="addItem__section">
 							<label
 								htmlFor="userList"
 								className="addItem__label label__select"
@@ -95,6 +97,7 @@ export function AddItem() {
 									value={selectedListToken}
 									onChange={updateSelectedList}
 									id="userList"
+									className="addItem__select addItem__select_listNames"
 								>
 									{getUserListsArr(userList).map(([name, token]) => (
 										<option key={token} value={token}>
